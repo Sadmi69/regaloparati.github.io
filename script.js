823ec1344c596d748ccbb2bb3275def5acf57e67
Sadmi69 <146953589+Sadmi69@users.noreply.github.com>
2024-02-14 13:44:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-document.getElementById('abrirRegalo').addEventListener('click', function() {
-    // Muestra el contenedor del regalo
-    document.getElementById('contenedorRegalo').style.display = 'block';
-  
-    // Crea elementos de tulipanes y gatos
-    var tulipanes = document.createElement('img');
-    tulipanes.src = 'https://github.com/Sadmi69/regaloparati.github.io/blob/main/script.js';  // Reemplaza 'ruta/tulipanes.gif' con la ruta correcta de tu imagen de tulipanes
-    tulipanes.alt = 'Tulipanes';
-    tulipanes.style.width = '300px';
-  
-    var gatos = document.createElement('img');
-    gatos.src = 'gatos.gif';  // Reemplaza 'ruta/gatos.gif' con la ruta correcta de tu imagen de gatos
-    gatos.alt = 'Gatos';
-    gatos.style.width = '300px';
-  
-    // Agrega elementos al contenedor
-    var contenedor = document.getElementById('contenedorRegalo');
-    contenedor.appendChild(tulipanes);
-    contenedor.appendChild(gatos);
-  });
-  
+$( document ).ready(function() {
+    
+    var envelope = $('#envelope');
+    var btn_open = $("#open");
+    var btn_reset = $("#reset");
+    
+    envelope.click( function() {
+        open();
+    });
+    btn_open.click( function() {
+        open();
+    });
+    btn_reset.click( function() {
+        close();
+    });
+
+    function open() {
+        envelope.addClass("open")
+           .removeClass("close");
+    }
+    function close() {
+        envelope.addClass("close")
+           .removeClass("open");
+    }
+   
+});
