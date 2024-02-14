cafbb702e02d64093fac6cc1a7ce9ea945615780
Sadmi69 <146953589+Sadmi69@users.noreply.github.com>
2024-02-14 13:32:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.getElementById('abrirRegalo').addEventListener('click', function() {
   
     // Crea elementos de tulipanes y gatos
     var tulipanes = document.createElement('img');
-    tulipanes.src = 'https://i.makeagif.com/media/1-17-2016/WlwwNV.gif';  // Reemplaza 'ruta/tulipanes.gif' con la ruta correcta de tu imagen de tulipanes
+    tulipanes.src = 'tulipanes.gif';  // Reemplaza 'ruta/tulipanes.gif' con la ruta correcta de tu imagen de tulipanes
     tulipanes.alt = 'Tulipanes';
     tulipanes.style.width = '300px';
   
     var gatos = document.createElement('img');
-    gatos.src = 'https://i.pinimg.com/originals/40/f2/42/40f242175a71e3b62fe717cad80e0cae.gif';  // Reemplaza 'ruta/gatos.gif' con la ruta correcta de tu imagen de gatos
+    gatos.src = 'gatos.gif';  // Reemplaza 'ruta/gatos.gif' con la ruta correcta de tu imagen de gatos
     gatos.alt = 'Gatos';
     gatos.style.width = '300px';
   
